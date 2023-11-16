d3733670edd263997b23d8757686af94ac5d48b0
padmasreegade <pgade@villanova.edu>
2023-11-16 16:58:09 -0500

    formatted

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-var http = require('http');
-const express = require('express');
+var http = require("http");
+const express = require("express");
 const app = express();
-var path = require('path');
+var path = require("path");
 var server = http.createServer(app);
 
-app.engine('.html', require('ejs').__express);
-app.set('views', __dirname);
-app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine(".html", require("ejs").__express);
+app.set("views", __dirname);
+app.set("view engine", "html");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-    res.render('index');});
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
-app.get('/index.html', function(req, res){
-    res.render('index');});
-    
-app.get('/education.html', function(req, res){
-    res.render('education.html');});
-    
-app.get('/experience.html', function(req, res){
-    res.render('experience.html');});
-    
-app.get('/certifications.html', function(req, res){
-    res.render('certifications.html');});
+app.get("/index.html", function (req, res) {
+  res.render("index");
+});
 
-app.get('/awards.html', function(req, res){
-    res.render('awards.html');});
-    
-app.get('/projects.html', function(req, res){
-    res.render('projects.html');});
-    
+app.get("/education.html", function (req, res) {
+  res.render("education.html");
+});
+
+app.get("/experience.html", function (req, res) {
+  res.render("experience.html");
+});
+
+app.get("/certifications.html", function (req, res) {
+  res.render("certifications.html");
+});
+
+app.get("/awards.html", function (req, res) {
+  res.render("awards.html");
+});
+
+app.get("/projects.html", function (req, res) {
+  res.render("projects.html");
+});
+
 server.listen(8080);
